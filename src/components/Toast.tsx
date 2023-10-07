31b4fb5cdeb66d6f886afc7745f75a331ebbdb6f
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-10-07 16:51:24 -0500

    Update toast position

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -10,14 +10,17 @@ export const Toast: React.FC<ToastProps> = ({ type, message }) => {
   const alertClass = type === 'success' ? 'alert-success' : 'alert-error';
 
   return (
-    <div className="toast toast-end">
+    <div
+      className={`fixed z-50 transform  top-24 left-1/2 -translate-y-1/2 -translate-x-1/2 md:translate-y-0 md:translate-x-0 md:top-14 md:right-5 md:left-auto`}
+    >
       <div
-        className={`flex flex-row items-center justify-center alert ${alertClass}`}
+        className={`flex flex-row items-center justify-center alert ${alertClass} text-sm md:text-base`}
       >
-        <div>
-          <Icon size={30} />
+        <div className="mr-2 md:mr-4">
+          <Icon size={20} className="md:text-xl" />
         </div>
-        <span>{message}</span>
+        <span className="hidden md:inline">{message}</span>
+        <span className="text-xs md:hidden">{message}</span>
       </div>
     </div>
   );
